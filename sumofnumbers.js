579567acfccc7eb5750e33a851797d24e68ac2e8
clyyoung <clyyoung@hawaii.edu>
2023-02-02 15:14:51 -1000

    attempt 1

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -1,3 +1,5 @@
+const testData = [1, 2, 3];
+
 function sumFor(data) {
   let sum = 0;
   for (const i of data) {
@@ -5,5 +7,28 @@ function sumFor(data) {
   }
   return sum;
 }
+console.log(sumFor(testData));
 
-console.log(sumFor([1, 2, 3]));
+function sumWhile(data) {
+  let sum = 0;
+  let i = 0;
+  while (i < data.length) {
+    sum += data[i];
+    i++;
+  }
+  return sum;
+}
+console.log(sumWhile(testData));
+
+function sumRecursion(data) {
+  if (data.length === 0) {
+    return 0;
+  }
+  return data[0] + sumRecursion(data.slice(1));
+}
+console.log(sumRecursion(testData));
+
+function sumTheSimpleWay(data) {
+  return _.reduce(data, function (memo, elem) { return memo + elem; });
+}
+console.log(sumTheSimpleWay(testData));
